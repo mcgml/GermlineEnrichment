1dd8268a90648469cad486fd9e70e6b36bc9b78f
mcgml <mcgml@cardiff.ac.uk>
2016-12-13 17:30:15 +0000

    bumped version

--- 2_GermlineEnrichment.sh
+++ 2_GermlineEnrichment.sh
@@ -8,7 +8,7 @@ cd $PBS_O_WORKDIR
 #Description: Germline Enrichment Pipeline (Illumina paired-end). Not for use with other library preps/ experimental conditions.
 #Author: Matt Lyon, All Wales Medical Genetics Lab
 #Mode: BY_COHORT
-version="1.0.3"
+version="1.0.4"
 
 #TODO SNPRelate
 #TODO PCA for ancestry
