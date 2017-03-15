9384eb5320c5f446a46de246025678507aa2d8d6
mcgml <mcgml@cardiff.ac.uk>
2017-03-15 11:55:09 +0000

    added TODO

--- 1_GermlineEnrichment.sh
+++ 1_GermlineEnrichment.sh
@@ -11,6 +11,7 @@ cd $PBS_O_WORKDIR
 version="1.6.0"
 
 # Script 1 runs in sample folder, requires fastq files split by lane
+#TODO exlude NTC
 
 countQCFlagFails() {
     #count how many core FASTQC tests failed
