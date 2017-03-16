1569ae359cde4dffc39b5cda30b02536a028d4c0
mcgml <mcgml@cardiff.ac.uk>
2017-03-16 13:20:01 +0000

    switched to GATK DOC for % gene coverage

--- 1_GermlineEnrichment.sh
+++ 1_GermlineEnrichment.sh
@@ -287,10 +287,8 @@ TMP_DIR=/state/partition1/tmpdir
 --countType COUNT_FRAGMENTS \
 --minMappingQuality 20 \
 --minBaseQuality 10 \
---omitIntervalStatistics \
---omitLocusTable \
 -ct "$minimumCoverage" \
--nt 12 \
+-geneList /data/db/human/refseq/geneTrack.refSeq.b37.sorted.txt.gz \
 -dt NONE
 
 #tabix index the per-base coverage file
@@ -298,17 +296,7 @@ awk -F'[\t|:]' '{if(NR>1) print $1"\t"$2"\t"$3}' "$seqId"_"$sampleId"_DepthOfCov
 /share/apps/htslib-distros/htslib-1.3.1/bgzip > "$seqId"_"$sampleId"_DepthOfCoverage.gz
 /share/apps/htslib-distros/htslib-1.3.1/tabix -b2 "$seqId"_"$sampleId"_DepthOfCoverage.gz
 
-#Calculate gene percentage coverage
-/share/apps/jre-distros/jre1.8.0_101/bin/java -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx16g -jar /data/diagnostics/apps/CoverageCalculator-2.0.2/CoverageCalculator-2.0.2.jar \
-"$seqId"_"$sampleId"_DepthOfCoverage \
-/data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/"$panel"/"$panel"_genes.txt \
-/state/partition1/db/human/refseq/ref_GRCh37.p13_top_level.gff3 \
--p5 \
--d"$minimumCoverage" \
-> "$seqId"_"$sampleId"_PercentageCoverage.txt
-
-#sort BED and add file prefix
-/share/apps/bedtools-distros/bedtools-2.26.0/bin/bedtools sort -i "$sampleId"_gaps.bed > "$seqId"_"$sampleId"_Coverage_Gaps.bed
+#TODO identify gaps
 
 #Extract 1kg autosomal snps for contamination analysis
 /share/apps/jre-distros/jre1.8.0_101/bin/java -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx4g -jar /share/apps/GATK-distros/GATK_3.7.0/GenomeAnalysisTK.jar \
