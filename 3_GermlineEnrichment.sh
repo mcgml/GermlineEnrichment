636b5928d3f8b5e27d52b899a1502461870dfe1e
mcgml <mcgml@cardiff.ac.uk>
2017-06-09 10:55:33 +0100

    fixed variable names

--- 3_GermlineEnrichment.sh
+++ 3_GermlineEnrichment.sh
@@ -192,15 +192,15 @@ addMetaDataToVCF "$seqId"_combined_filtered.vcf
 ### ROH, SV & CNV analysis ###
 
 #identify runs of homozygosity
-for sampleId in $(/share/apps/bcftools-distros/bcftools-1.4/bcftools query -l "$seqId"_combined_filtered_meta.vcf); do
+for sample in $(/share/apps/bcftools-distros/bcftools-1.4/bcftools query -l "$seqId"_combined_filtered_meta.vcf); do
 
     #make >min coverage BED
-    zcat "$sampleId"/"$seqId"_"$sampleId"_DepthOfCoverage.gz | \
+    zcat "$sample"/"$seqId"_"$sample"_DepthOfCoverage.gz | \
     awk -vminimumCoverage="$minimumCoverage" '$3 >= minimumCoverage {print $1"\t"$2-1"\t"$2}' | \
-    /share/apps/bedtools-distros/bedtools-2.26.0/bin/bedtools merge > "$sample"/"$seqId"_"$sampleId"_gt_eq_"$minimumCoverage".bed
+    /share/apps/bedtools-distros/bedtools-2.26.0/bin/bedtools merge > "$sample"/"$seqId"_"$sample"_gt_eq_"$minimumCoverage".bed
     
     #calculate LOH
-    /share/apps/bcftools-distros/bcftools-1.4/bcftools roh -O r -s "$sample" -R "$sample"/"$seqId"_"$sampleId"_gt_eq_"$minimumCoverage".bed "$seqId"_combined_filtered_meta.vcf.gz | \
+    /share/apps/bcftools-distros/bcftools-1.4/bcftools roh -O r -s "$sample" -R "$sample"/"$seqId"_"$sample"_gt_eq_"$minimumCoverage".bed "$seqId"_combined_filtered_meta.vcf.gz | \
     grep -v '^#' | awk '{print $3"\t"$4-1"\t"$5"\t\t"$8}' > "$sample"/"$seqId"_"$sample"_roh.bed
 
 done
