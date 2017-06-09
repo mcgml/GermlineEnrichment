53ccf6033addfd1724dadcdc8399728fbc8d3566
mcgml <mcgml@cardiff.ac.uk>
2017-06-09 10:46:07 +0100

    updated refinement scipt

--- GenotypeRefinement.sh
+++ GenotypeRefinement.sh
@@ -21,26 +21,36 @@ version="2.0.0"
 /share/apps/jre-distros/jre1.8.0_131/bin/java -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10 -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx4g -jar /share/apps/GATK-distros/GATK_3.7.0/GenomeAnalysisTK.jar \
 -T CalculateGenotypePosteriors \
 -R /state/partition1/db/human/gatk/2.8/b37/human_g1k_v37.fasta \
--V variants.vcf \
+-V "$seqId"_combined_filtered_100pad.vcf \
 --skipPopulationPriors \
 -ped "$seqId"_pedigree.ped \
 -L /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/"$panel"/"$panel"_ROI_b37.bed \
 -ip 100 \
--o output.withPosteriors.vcf
+-o "$seqId"_combined_filtered_100pad_GCP.vcf \
+-dt NONE
 
 #filter genotypes
 /share/apps/jre-distros/jre1.8.0_131/bin/java -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10 -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx4g -jar /share/apps/GATK-distros/GATK_3.7.0/GenomeAnalysisTK.jar \
 -T VariantFiltration \
 -R /state/partition1/db/human/gatk/2.8/b37/human_g1k_v37.fasta \
--V output.withPosteriors.vcf \
+-V "$seqId"_combined_filtered_100pad_GCP.vcf \
+-ped "$seqId"_pedigree.ped \
 --genotypeFilterExpression "DP < 10" \
 --genotypeFilterName "LowDP" \
 --genotypeFilterExpression "GQ < 20" \
 --genotypeFilterName "LowGQ" \
---setFilteredGtToNocall \
 -L /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/"$panel"/"$panel"_ROI_b37.bed \
 -ip 100 \
--o "$seqId"_non_snps_filtered.vcf \
+-o "$seqId"_combined_filtered_100pad_GCP_filtered.vcf \
 -dt NONE
 
 #phase genotypes
+/share/apps/jre-distros/jre1.8.0_131/bin/java -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10 -Djava.io.tmpdir=/state/partition1/tmpdir -Xmx4g -jar /share/apps/GATK-distros/GATK_3.7.0/GenomeAnalysisTK.jar \
+-T PhaseByTransmission \
+-R /state/partition1/db/human/gatk/2.8/b37/human_g1k_v37.fasta \
+-V "$seqId"_combined_filtered_100pad_GCP_filtered.vcf \
+-ped "$seqId"_pedigree.ped \
+-o "$seqId"_combined_filtered_100pad_GCP_filtered_phased.vcf \
+-L /data/diagnostics/pipelines/GermlineEnrichment/GermlineEnrichment-"$version"/"$panel"/"$panel"_ROI_b37.bed \
+-ip 100 \
+-dt NONE
